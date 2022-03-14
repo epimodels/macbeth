117cb4bdf54a1f21462187f650bbb3939956b4ae
Ethan <ethannelson432@gmail.com>
2022-03-14 15:07:34 -0700

    Basic user input and output

--- macbeth/django_project/settings.py
+++ macbeth/django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'macbeth',
+        'USER': 'macbeth',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
