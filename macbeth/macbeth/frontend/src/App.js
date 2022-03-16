4aa62ed8542773bc320cc7a9e965c3470717c962
Mikaela <mikaela.dean@wsu.edu>
2022-03-15 17:59:33 -0700

    added footer component

--- macbeth/macbeth/frontend/src/App.js
+++ macbeth/macbeth/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import WebNavbar from './components/webnavbar'
+import Footer from './components/footer'
 
 class App extends React.Component {
   constructor(props) {
@@ -17,6 +18,7 @@ class App extends React.Component {
         <header className="App-header">
           
         </header>
+        <Footer />
       </div>
     );
   }
