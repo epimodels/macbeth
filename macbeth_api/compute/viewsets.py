7eecd3c815ad4fb4d4aeb2366a2843a7ffb343ae
Kyle Hurd <kyle.hurd@wsu.edu>
2022-05-01 19:02:50 -0700

    Model list to a dictionary

--- macbeth_api/compute/viewsets.py
+++ macbeth_api/compute/viewsets.py
@@ -2,6 +2,18 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from macbeth_backend import COMPUTE_MODELS
+from macbeth_backend.computations.config import Config
+
+# [{ Title, Version, id }, ...]
+# list() = GET "API/Compute/"
+
+# retrieve = GET "API/Compute/ID"
+
+# create = POST "API/Compute/"
+MODEL_DICT = {}
+for model in COMPUTE_MODELS:
+    MODEL_DICT[model['name']] = model
 
 
 class ComputeModelsViewSet(viewsets.ViewSet):
@@ -13,49 +25,19 @@ class ComputeModelsViewSet(viewsets.ViewSet):
     def list(self, request):
         '''List all available compute models.'''
         return Response({
-            'models':
-            [
-                {"name": "Example Model 1", "id": "examplemodel1"},
-                {"name": "Example Model 2", "id": "examplemodel2"},
-                {"name": "Example Model 3", "id": "examplemodel3"},
-            ]
+            'models': MODEL_DICT,
         }, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk=None):
         '''Retrieve a specific compute model.'''
-        return Response({
-                        "Title": "Example Model",
-                        "Version": "1.0",
-                        "Author": "Example Author",
-                        "Description": "This is an example model that demonstrates how the different functionality works for configuring epidemic models",
-                        "Type": "Deterministic",
-                        "Authorlink": "https://www.idmod.org/",
-                        "Parameters":
-                        [
-                            {
-                                "Name": "Integer Example",
-                                "Type": "integer",
-                                "Description": "A brief explanation of what the parameter controls.",
-                                "Min": 0,
-                                "Max": 10000,
-                                "DefaultValue": 100,
-                            },
-                            {
-                                "Name": "Double Example",
-                                "Type": "double",
-                                "Description": "A brief explanation of what the parameter controls.",
-                                "Min": -1.0,
-                                "Max": 1,
-                                "DefaultValue": 0.1
-                            },
-                            {
-                                "Name": "String Example",
-                                "Type": "string",
-                                "Description": "A brief explanation of what the parameter controls. Strings might be used for seeds.",
-                                "Min": {},
-                                "Max": {},
-                                "DefaultValue": "",
-                            },
-                        ],
-                        },
-                        status=status.HTTP_200_OK)
+        # The identifier is the name of the model class
+        # pk = hopefully the id of the model we want.
+        # Make COMPUTE_MODELS a dictionary of {name: model}
+        # if pk in dict, return the config file for it, otherwise error
+        print(f'pk: {pk}')
+        return Response(
+            Config.load_config_from_obj(
+                MODEL_DICT['ZombieSIR']['model'],
+            ),
+            status=status.HTTP_200_OK,
+        )
