c1c7707cc6d0ca9dd6b5b94f148f92a7a65d944c
mikalooloo <mikaeladean10@gmail.com>
2022-09-08 17:17:38 -0700

    Created a test line graph to understand how we could use Chart.js to display results, can view this on the Results page

--- macbeth_frontend/src/components/model_results/results.js
+++ macbeth_frontend/src/components/model_results/results.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import NavButton from '../nav_button'
-import Progress from '../compute/progress'
+import NavButton from '../nav_button';
+import Progress from '../compute/progress';
+import ResultsGraph from './results_graph';
 
 /*
  * Sub-page of Compute
  */
 export default function Results() {
+  const datasets = [
+    {
+      'label': 'Testing Label',
+      'data': [(0, 0), (1, 1), (2, 2)],
+      'borderColor': 'rgb(255, 99, 132)',
+      'backgroundColor': 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      'label': 'Testing Label2',
+      'data': [(2, 2), (1, 1), (0, 0)],
+      'borderColor': 'rgb(53, 162, 235)',
+      'backgroundColor': 'rgba(53, 162, 235, 0.5)',
+    }
+  ]
+
+
   return (
     <div>
       <Progress currentStep={3} />
       <h4>Viewing Results</h4>
+      <div style={{'width':'50%', 'height':'50%', 'paddingLeft':'5%'}}><ResultsGraph title={"Test"} datasets={datasets} /></div>
       <NavButton label='Back' redirect='/compute/parameter-edit' variant='prev'/>
     </div>
   )
---
+++ macbeth_frontend/src/components/model_results/results_graph.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Chart as ChartJS, 
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend, } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+);
+
+
+/*
+ * Displays the graph when showing off results
+ * Parameters:
+ * Title (title of graph)
+ * Datasets (list of objects with a 'label' (legend key), 'data', 'borderColor', and 'backgorundColor')
+ * 
+ */
+export default function ResultsGraph(props) {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: props.title,
+            },
+        },
+    };
+
+    const data = {
+        labels: [0, 1, 2],
+        datasets: props.datasets.map((dataset) =>
+        (
+            {
+                label: dataset.label,
+                data: dataset.data,
+                borderColor: dataset.borderColor,
+                backgroundColor: dataset.backgroundColor,
+            }
+        ))
+    };
+
+    return (
+      <Line options={options} data={data} />
+    )
+  }
+
+  /*
+  props.datasets.map((dataset) =>
+            (
+                {
+                    label: dataset.label,
+                    data: [(0, 0), (1, 1), (2, 2)],
+                    borderColor: dataset.borderColor,
+                    backgroundColor: dataset.backgroundColor,
+                }
+            ))*/
