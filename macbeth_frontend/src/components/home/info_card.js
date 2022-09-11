2343f3950c502052c0c0da18a1ab23405830166b
mikalooloo <mikaeladean10@gmail.com>
2022-09-11 14:16:07 -0700

    Fixed warnings/errors showing up in the console; removed Card.Text from InfoCard and UpdateLogCard since it implicitly includes <p> which brings up an error when working with lists inside, updated some in-line styling names

--- macbeth_frontend/src/components/home/info_card.js
+++ macbeth_frontend/src/components/home/info_card.js
@@ -20,7 +20,6 @@ class InfoCard extends React.Component {
         <Card.Body>
           <Card.Title>New to MACBETH?</Card.Title>
           <Card.Subtitle className='mb-2 text-muted'></Card.Subtitle>
-          <Card.Text>
             <Accordion defaultActiveKey="0"> {/* would be cool to implement if signed in, no default key, but if not, then this is active */}
               <Accordion.Item className={'accordion-' + this.state.theme} eventKey="0">
                 <Accordion.Header>About MACBETH</Accordion.Header>
@@ -62,7 +61,6 @@ class InfoCard extends React.Component {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>       
-          </Card.Text>
         </Card.Body>
       </Card>
     )
