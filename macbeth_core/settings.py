77460b25dd80a305189f7b2f9f230900fd1aca5a
Trevor N <NullSnapshot@users.noreply.github.com>
2022-11-15 10:41:35 -0800

    Candidate patch for stochpy

--- macbeth_core/settings.py
+++ macbeth_core/settings.py
@@ -25,6 +25,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJ_DIR = os.getcwd()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
